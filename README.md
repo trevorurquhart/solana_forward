5dfe4550c6ecab7bae18af101ac499d0b5049640
urquhartt <urquhartt@lmax.com>
2024-05-03 14:45:47 +1200

    basic test and program running

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Solana Forward
+
+### How to run locally
+
+#### Install tools
+
+Instructions on how to set up a local environment can be found [here](https://solana.com/developers/guides/getstarted/setup-local-development).
+
+#### Build
+```bash
+# Install dependencies
+$ yarn
+
+# Build
+$ cargo build
+
+# Test
+$ yarn run test
+
+# Deploy to local validator
+
+$ solana program deploy ./target/deploy/solana_forward.so
+
+# Start local validator:
+$ solana-test-validator
+
+# Watch local validator logs
+$ solana logs --url localhost
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+entrypoint!(process_instruction);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+    // log a message to the blockchain
+    msg!("Hello, world!");
+    msg!("Our program's Program ID: {}", &program_id);
+    // gracefully exit the program
+    Ok(())
 }
