6cbd5235a38ca1855666e1736e461dcd149a2bee
urquhartt <urquhartt@lmax.com>
2024-06-05 07:51:44 +1200

    unwip all the execution tests

--- childpda/tests/execute.ts
+++ childpda/tests/execute.ts
@@ -33,8 +33,10 @@ describe("execute instruction tests", () => {
     it("Should transfer sol when executed", async () => {
 
         let forwardAmount = LAMPORTS_PER_SOL / 100;
+        await deposit(connection, payer, destination.publicKey, LAMPORTS_PER_SOL * 3)
         await deposit(connection, payer, forwardPda, forwardAmount);
 
+        let destinationBalanceBefore = await connection.getBalance(destination.publicKey);
         try {
             await execute(payer, program, connection, forwardAccount, forwardPda, destination)
         } catch (e) {
@@ -42,7 +44,7 @@ describe("execute instruction tests", () => {
         }
         let destinationBalanceAfter = await connection.getBalance(destination.publicKey);
 
-        expect(destinationBalanceAfter).to.equal(forwardAmount);
+        expect(destinationBalanceAfter - destinationBalanceBefore).to.equal(forwardAmount);
     });
 
     it("Should transfer tokens when executed", async () => {
@@ -103,49 +105,49 @@ describe("execute instruction tests", () => {
         const info = await connection.getTokenAccountBalance(destinationAta);
         expect(info.value.uiAmount).to.equal(destinationTokenBalanceBefore);
     });
-    //
-    // it("Execute should forward sol and multiple tokens", async () => {
-    //     const mintAuthority2 = Keypair.generate();
-    //     let mint2 = await createMint(connection, payer, mintAuthority2.publicKey, null, 0);
-    //
-    //     let solAmount = LAMPORTS_PER_SOL / 200;
-    //     let token1Amount = 300;
-    //     let token2Amount = 400;
-    //
-    //     const destSolBalanceBefore = await connection.getBalance(destination.publicKey);
-    //
-    //     await deposit(connection, payer, forwardPda, solAmount);
-    //     let fwdAtaToken1 = await createAndFundAta(connection, payer, mint, mintAuthority, token1Amount, forwardPda);
-    //     let fwdAtaToken2 = await createAndFundAta(connection, payer, mint2, mintAuthority2, token2Amount, forwardPda);
-    //
-    //     let destAtaToken1 = await createAndFundAta(connection, payer, mint, mintAuthority, 0, destination.publicKey);
-    //     let destAtaToken2 = await createAndFundAta(connection, payer, mint2, mintAuthority2, 0, destination.publicKey);
-    //
-    //     try {
-    //         await executeWithTokens(payer, program, connection, forwardPda, destination, TOKEN_PROGRAM_ID, mint, fwdAtaToken1, destAtaToken1, mint2, fwdAtaToken2, destAtaToken2);
-    //     } catch (e) {
-    //         console.error(e);
-    //     }
-    //     const destSolBalance = await connection.getBalance(destination.publicKey);
-    //     const destToken1Balance = (await connection.getTokenAccountBalance(destAtaToken1)).value.uiAmount;
-    //     const destToken2Balance = (await connection.getTokenAccountBalance(destAtaToken2)).value.uiAmount;
-    //
-    //     expect(destSolBalance - destSolBalanceBefore, "sol balance").to.equal(solAmount);
-    //     expect(destToken1Balance, "token 1 balance").to.equal(token1Amount);
-    //     expect(destToken2Balance, "token 2 balance").to.equal(token2Amount);
-    // });
-    //
-    // it("should create a token account for the destination if one does not exist", async () => {
-    //
-    //     let tokenAmount = 1000;
-    //     let forwardAta = await createAndFundAta(connection, payer, mint, mintAuthority, tokenAmount, forwardPda);
-    //     const uninitialised = await getAssociatedTokenAddressSync(mint, destination.publicKey);
-    //     try {
-    //         await executeWithTokens(payer, program, connection, forwardPda, destination, TOKEN_PROGRAM_ID, mint, forwardAta, uninitialised);
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
-    //     const info = await connection.getTokenAccountBalance(uninitialised);
-    //     expect(info.value.uiAmount).to.equal(tokenAmount);
-    // });
+
+    it("Execute should forward sol and multiple tokens", async () => {
+
+        const mintAuthority2 = Keypair.generate();
+        let mint2 = await createMint(connection, payer, mintAuthority2.publicKey, null, 0);
+
+        let solAmount = LAMPORTS_PER_SOL / 200;
+        let token1Amount = 300;
+        let token2Amount = 400;
+
+        await deposit(connection, payer, forwardPda, solAmount);
+        let fwdAtaToken1 = await createAndFundAta(connection, payer, mint, mintAuthority, token1Amount, forwardPda);
+        let fwdAtaToken2 = await createAndFundAta(connection, payer, mint2, mintAuthority2, token2Amount, forwardPda);
+
+        const destAtaToken1 = await getAssociatedTokenAddressSync(mint, destination.publicKey);
+        const destAtaToken2 = await getAssociatedTokenAddressSync(mint2, destination.publicKey);
+
+
+        try {
+            await executeWithTokens(payer, program, connection, forwardAccount, forwardPda, destination, TOKEN_PROGRAM_ID, mint, fwdAtaToken1, destAtaToken1, mint2, fwdAtaToken2, destAtaToken2);
+        } catch (e) {
+            console.error(e);
+        }
+        const destSolBalance = await connection.getBalance(destination.publicKey);
+        const destToken1Balance = (await connection.getTokenAccountBalance(destAtaToken1)).value.uiAmount;
+        const destToken2Balance = (await connection.getTokenAccountBalance(destAtaToken2)).value.uiAmount;
+
+        expect(destSolBalance, "sol balance").to.equal(solAmount);
+        expect(destToken1Balance, "token 1 balance").to.equal(token1Amount);
+        expect(destToken2Balance, "token 2 balance").to.equal(token2Amount);
+    });
+
+    it("should create a token account for the destination if one does not exist", async () => {
+
+        let tokenAmount = 1000;
+        let forwardAta = await createAndFundAta(connection, payer, mint, mintAuthority, tokenAmount, forwardPda);
+        const uninitialised = await getAssociatedTokenAddressSync(mint, destination.publicKey);
+        try {
+            await executeWithTokens(payer, program, connection, forwardAccount, forwardPda, destination, TOKEN_PROGRAM_ID, mint, forwardAta, uninitialised);
+        } catch (e) {
+            console.log(e)
+        }
+        const info = await connection.getTokenAccountBalance(uninitialised);
+        expect(info.value.uiAmount).to.equal(tokenAmount);
+    });
 });
